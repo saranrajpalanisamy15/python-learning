87bd2624664b4c6ccff6edfa83bf55cea9913816
saranrajpalanisamy15 <saranraj.palanisamy15@gmail.com>
2026-05-19 11:48:01 +0530

    Added string methods practice

---
+++ string/methods.py
@@ -0,0 +1,14 @@
+#STRING METHODS 
+text=" hello python "
+
+print(text.upper())
+print(text.lower())
+print(text.strip())
+print(text.replace("python","world"))
+print(text.split())
+print(text.find("python"))
+print(text.count("o"))
+print(text.startswith(" "))
+print(text.endswith(" "))
+print(text.title())
+print(text.capitalize())
