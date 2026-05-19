f34d7e79eb21ba216ae31b55b9a59f058241afbd
Psaranraj <146187902+Psaranraj@users.noreply.github.com>
2026-05-19 15:56:29 +0530

    added prime number example

---
+++ example/test.py
@@ -0,0 +1,12 @@
+num=int(input("Enter a number: " ))
+count = 0
+if num > 0:
+    for fact in range(1, num+1):
+        if num % fact == 0:
+            count += 1
+    if count == 2:
+        print(f"{num} is a prime number.")
+    else:
+        print(f"{num} is not a prime number.")
+else:
+    print(f"{num} is not a prime number.")
